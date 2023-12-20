169691aad4b561c44620f2110894677765b51df2
Taaran Jain <taaranjain@Taarans-MacBook-Air.local>
2023-12-20 17:07:56 +0530

    Second Commit

--- src/university/management/system/About.java
+++ src/university/management/system/About.java
@@ -10,7 +10,7 @@ public class About extends JFrame {
         setLocation(400, 150);
         getContentPane().setBackground(Color.WHITE);
 
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/about.jpg"));
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource(""));
         Image i2 = i1.getImage().getScaledInstance(300, 200, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel image = new JLabel(i3);
